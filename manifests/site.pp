6d055a1e8a2e68a936eabe8aad17c33f5c898fda
mwarnick123 <71853447+mwarnick123@users.noreply.github.com>
2023-01-18 15:17:30 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,2 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is a new readme file',
-    owner   => 'root',
-  }
-  file {'/root/README':
-    owner => 'root',
-  }
 }
